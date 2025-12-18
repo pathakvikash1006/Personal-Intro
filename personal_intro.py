382c1049bf280b3f10eb59ac65b81f0fe5665937
Vikash Pathak <pathakvikash1006@gmail.com>
2025-12-18 14:55:54 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,13 @@
+#Personal Introduction Script
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")   
+hobby = input("Enter your favorite hobby: ")
+
+print("\nPersonal Introduction:")
+print(f"Hello! My name is {name}.")
+print(f"I am {age} years old and I live in {city}.")    
+print(f"My favorite hobby is {hobby}.")
+print("Nice to meet you!")
+
+# End of personal_intro.py
